8eb0109bb4f83aaad6baf57ccb79fe1ceacecb34
unknown <max.putilov@gmail.com>
2015-06-01 22:29:10 +0300

    fix build

--- dist/index.js
+++ dist/index.js
@@ -74,7 +74,7 @@ var logger;
             this.insertAsync = Promise.promisify(this.db[mongoOpts.collection].insert, this.db[mongoOpts.collection]);
         }
         LoggerMongo.prototype.write = function (obj) {
-            var doc = { tags: this.tags, msg: obj };
+            var doc = { tags: this.tags, msg: obj, date: new Date() };
             return this.insertAsync(doc);
         };
         return LoggerMongo;
